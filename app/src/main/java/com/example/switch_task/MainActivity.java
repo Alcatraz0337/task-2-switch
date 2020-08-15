457c7f538904d0bd0f64282181c898bd86fcad04
Alcatraz0337 <divyam95@gmail.com>
2020-08-16 00:26:16 +0200

    Created two switch objects with the first one having light/dark theme type functionality

--- app/src/main/java/com/example/switch_task/MainActivity.java
+++ app/src/main/java/com/example/switch_task/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.switch_task;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
+
+    Switch switchA, switchB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        switchA = (Switch) findViewById(R.id.switchA);
+        switchB = (Switch) findViewById(R.id.switchB);
+
+        final View view2 = findViewById(R.id.abc);
+        //view2.setBackgroundColor(getResources().getColor(android.R.color.background_dark));
+
+        switchA.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                if (switchA.isChecked())
+                    view2.setBackgroundColor(getResources().getColor(android.R.color.background_dark));
+                else
+                    view2.setBackgroundColor(getResources().getColor(android.R.color.background_light));
+            }
+        });
+
+        //switchB.setOnClickListener();
     }
 }
